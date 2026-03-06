138beaead5a8cb078191c662b33b8a3b08d4374c
kamatealif <kamatealip1@gmail.com>
2026-03-06 20:08:00 +0530

    one hot encoding from scratch

--- tf-idf/one_hot_encoding.py
+++ tf-idf/one_hot_encoding.py
@@ -1,25 +1,68 @@
-
-
-
-class OneHotEncoding():
-    def __init__(self,data) -> None:
+class OneHotEncoding:
+    def __init__(self, data):
         self.data = data
-    
-    def corpus(self):
-        corpus = list()
+        self._corpus = self._build_corpus()
+        self._vocabulary = self._build_vocabulary()
+        self._word_to_index = {
+            word: index for index, word in enumerate(self._vocabulary)
+        }
+
+    def _build_corpus(self):
+        corpus = []
         for sentence in self.data:
-            for word in sentence.split():
-                corpus.append(word)
+            corpus.extend(sentence.split())
         return corpus
-    
+
+    def _build_vocabulary(self):
+        # Keep insertion order so indices are stable and predictable.
+        seen = set()
+        vocabulary = []
+        for word in self._corpus:
+            if word not in seen:
+                seen.add(word)
+                vocabulary.append(word)
+        return vocabulary
+
+    def corpus(self):
+        return self._corpus.copy()
+
     def vocabulary(self):
-        
-        return list(set(self.corpus()))
+        return self._vocabulary.copy()
 
-data = ['people watch youtube', 'youtube watch people','people comment on youtube', 'youtube comment on people']
+    def one_hot_vector(self, word):
+        vector = [0] * len(self._vocabulary)
+        index = self._word_to_index.get(word)
+        if index is not None:
+            vector[index] = 1
+        return vector
 
-one_hot = OneHotEncoding(data)
-print(one_hot.data)
+    def encode_sentence(self, sentence):
+        return [self.one_hot_vector(word) for word in sentence.split()]
 
-print("Corpus:", one_hot.corpus())
-print("Vocabulary:", one_hot.vocabulary())
+    def multi_hot_vector(self, sentence):
+        vector = [0] * len(self._vocabulary)
+        for word in sentence.split():
+            index = self._word_to_index.get(word)
+            if index is not None:
+                vector[index] = 1
+        return vector
+
+
+if __name__ == "__main__":
+    data = [
+        "people watch youtube",
+        "youtube watch people",
+        "people comment on youtube",
+        "youtube comment on people",
+    ]
+
+    encoder = OneHotEncoding(data)
+    print("Corpus:", encoder.corpus())
+    print("Vocabulary:", encoder.vocabulary())
+
+    sentence = data[0]
+    print(
+        f"One-hot vectors for words in '{sentence}': "
+        f"{encoder.encode_sentence(sentence)}"
+    )
+    print(f"Multi-hot vector for '{sentence}': {encoder.multi_hot_vector(sentence)}")
